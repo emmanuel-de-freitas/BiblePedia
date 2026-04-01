f9dec98bb4fd98e6047bfc9d687e496c0e0319e1
Emmanuel De Freitas <manny.defreitas7@me.com>
2026-04-01 01:06:58 -0700

    Remove locale detection and simplify RootLayout

--- apps/livrea/src/app/layout.tsx
+++ apps/livrea/src/app/layout.tsx
@@ -1,22 +1,22 @@
 import type { Metadata } from "next";
-// app/layout.tsx
 import type * as React from "react";
 import { Providers } from "./providers";
-import { usePreferredLanguage } from "@uidotdev/usehooks";
 
 export const metadata: Metadata = {
   description: "Your personal ebook reader and listener",
   title: "Livrea | Ebook listener",
 };
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  // Get the user's preferred language from the Accept-Language header.
-  // You could also get this from a database, URL param, etc.
-  const lang = usePreferredLanguage();
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <Providers locale={lang}>
-      <body>{children}</body>
-    </Providers>
+    <html lang="en" suppressHydrationWarning>
+      <Providers>
+        <body>{children}</body>
+      </Providers>
+    </html>
   );
 }
