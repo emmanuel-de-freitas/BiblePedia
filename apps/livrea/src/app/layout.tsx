6c4e7a00a9e41140ecab369a877f77a7508a7f32
Emmanuel De Freitas <manny.defreitas7@me.com>
2026-04-01 19:23:36 -0700

    Convert layout and page to client components

--- apps/livrea/src/app/layout.tsx
+++ apps/livrea/src/app/layout.tsx
@@ -1,11 +1,8 @@
-import type { Metadata } from "next";
+"use client";
+
+
 import type * as React from "react";
 import { Providers } from "./providers";
-
-export const metadata: Metadata = {
-  description: "Your personal ebook reader and listener",
-  title: "Livrea | Ebook listener",
-};
 
 export default function RootLayout({
   children,
--- apps/livrea/src/app/page.tsx
+++ apps/livrea/src/app/page.tsx
@@ -1,22 +1,18 @@
+"use client";
+
 //import { useEffect } from "react";
-//import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Entry() {
-   //const router = useRouter();
+  const router = useRouter();
 
-   // useEffect(() => {
-   //    router.replace("/dashboard");
-   // }, [router]);
+  // useEffect(() => {
+  router.replace("/dashboard");
+  // }, [router]);
 
-   return (
-      <div
-         style={{
-            alignItems: "center",
-            display: "flex",
-            height: "100vh",
-            justifyContent: "center",
-         }}>
-         <p>Loading...</p>
-      </div>
-   );
+  return (
+    <div>
+      <p>Loading...</p>
+    </div>
+  );
 }
