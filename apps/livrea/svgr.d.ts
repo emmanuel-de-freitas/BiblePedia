60ee14c9b6f905006874c9489d018614581eb452
Emmanuel De Freitas <manny.defreitas7@me.com>
2026-04-05 17:26:14 -0700

    update imgs

--- apps/livrea/svgr.d.ts
+++ apps/livrea/svgr.d.ts
@@ -8,11 +8,3 @@ declare module '*.svg?url' {
   const content: any
   export default content
 }
-
-
-// Configure the type of the `routerOptions` prop on all React Spectrum components.
-declare module '@react-spectrum/s2' {
-  interface RouterConfig {
-    routerOptions: NonNullable<Parameters<ReturnType<typeof useRouter>['push']>[1]>
-  }
-}
